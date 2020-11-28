3cd19f245f7ee46a7bfd705078e32d25a95624bd
mccright <matt.mccright@gmail.com>
2020-11-27 19:05:41 -0600

    Initial commit

--- script-template.sh
+++ script-template.sh
@@ -5,19 +5,20 @@
 ##       -h --help               This text
 
 BASE=$(cd "$(dirname "$0")" || exit; pwd -P)
+# Use our logging module -- 
+# The logging script must be in your path. Correct the path if needed for your environment.
+logscript=`which logto.sh`
+source $logscript ; ret=$?
+if [[ $ret -eq 1 ]] ; then
+  echo "$HOME/bin/logto.sh not found. Exiting." ; exit $ret
+fi
+PROG=` basename $0 `
+thisScript="${0}"
+logTag="tagForTesting"
 
 usage() {
    grep "^## " "${BASH_SOURCE[0]}" | cut -c 4-
    exit 0
-}
-
-warn() {
-   cat <<< "$@" 1>&2
-}
-
-error() {
-   cat <<< "$@" 1>&2
-   exit 1
 }
 
 [[ $# == 0 ]] && usage
@@ -44,3 +45,16 @@ done
 if [[ -n "$EXAMPLE" ]]; then
    example
 fi
+
+# Here is a section demonstrating an approach to logging - Delete if not needed
+log "${0} - This is a test.  Logging an LOG message ${logTag}"
+logTag="StartOfSomethingNotable"
+info "${0} - This is a test.  Logging an INFO message ${logTag}"
+logTag=""
+warn "${0} - This is a test.  Logging an WARN message ${logTag}"
+logTag="New function for input"
+error "${0} - This is a test.  Logging an ERROR message ${logTag}"
+logTag="StartOfDebuggingSection"
+debug "${0} - This is a test.  Logging an DEBUG message ${logTag}"
+logTag=""
+
