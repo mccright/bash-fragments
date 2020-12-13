d58cc1aba348e0652fbdda1af804f06017132005
mccright <matt.mccright@gmail.com>
2020-12-13 09:20:13 -0600

    Initial commit

---
+++ current-os.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# SUMMARY: Describe the current OS
+# OUTPUT EXAMPLE: 
+# Current OS: Ubuntu 20.04.1 LTS 'focal' (GNU/Linux 5.4.0-58-generic x86_64)
+# WHY: I visit a lot of different hosts throughout any given day.
+# The OS running on many of those hosts are managed by others.
+# Putting this in my .bashrc helps remind me of an important 
+# characteristic of the current server upon login.
+# HOW: In my case, I add the following (uncomment the .bashrc code):
+# # What OS is running?
+# if [ -f ~/bin/current-os.sh ]; then
+#     . ~/bin/current-os.sh
+# fi
+#
+SHORT_DISTRIB_DESC=$(lsb_release -s -d)
+CODE_NAME=$(lsb_release -c)
+#
+printf "Current OS: %s \'%s\' (%s %s %s)\n" "$SHORT_DISTRIB_DESC" "$(echo $CODE_NAME | cut -d " " -f 2)" "$(uname -o)" "$(uname -r)" "$(uname -m)"
